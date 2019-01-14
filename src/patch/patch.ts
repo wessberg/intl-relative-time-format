20bcd1eabd4d918fe18523a9319379a86badf915
wessberg <frederikwessberg@gmail.com>
2019-01-15 00:11:54 +0100

    feat(chore): better error messages

--- src/patch/patch.ts
+++ src/patch/patch.ts
@@ -5,7 +5,7 @@ import {RelativeTimeFormat} from "../relative-time-format/relative-time-format/r
  */
 export function patch (): void {
 	if (typeof Intl === "undefined") {
-		throw new TypeError(`Could not define Intl.RelativeTimeFormat: Expected 'Intl' to exist. Remember to include polyfills for Intl.NumberFormat and Intl.PluralRules before applying this polyfill`);
+		throw new TypeError(`Could not define Intl.RelativeTimeFormat: Expected 'Intl' to exist. Remember to include polyfills for Intl.NumberFormat, Intl.getCanonicalLocales, and Intl.PluralRules before applying this polyfill`);
 	}
 	Intl.RelativeTimeFormat = RelativeTimeFormat;
 }
