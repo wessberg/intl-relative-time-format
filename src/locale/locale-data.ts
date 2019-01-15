673e964e7cd2a5f03d8aad27eab943ab669ca0e5
wessberg <frederikwessberg@gmail.com>
2019-01-15 22:55:14 +0100

    fix(chore): Removes a few @ts-ignore comments

--- src/locale/locale-data.ts
+++ src/locale/locale-data.ts
@@ -2,14 +2,10 @@ import {Locale} from "./locale";
 import {ExtendedSingularRelativeTimeUnit} from "../unit/singular-relative-time-unit";
 import {RelevantExtensionKey} from "../relevant-extension-key/relevant-extension-key";
 
-export interface LocaleDataEntryValue {
-	[key: string]: typeof key extends "future"|"past" ? Record<string, string> : string;
-
-	// @ts-ignore
+export type LocaleDataEntryValue = {
 	future: Record<string, string>;
-	// @ts-ignore
 	past: Record<string, string>;
-}
+} & {[key: string]: string};
 
 export type LocaleDataEntry = {
 	[Key in ExtendedSingularRelativeTimeUnit|RelevantExtensionKey]: Key extends ExtendedSingularRelativeTimeUnit ? LocaleDataEntryValue|undefined : string[]|undefined;
